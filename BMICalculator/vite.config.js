14ed16c616d26f6da5cae3e5bd5d27d79574162e
Lajpat Rai <140961591+Lajpat2550@users.noreply.github.com>
2025-04-29 23:51:56 +0500

    Add files via upload
    files have been uploaded

---
+++ BMICalculator/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+<<<<<<< HEAD
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(),
+=======
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),    tailwindcss(),
+>>>>>>> c6e94a3 (Add multiple task files to the folder)
+  ],
+})
